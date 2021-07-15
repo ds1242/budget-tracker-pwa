77ca98b52cb81b0cb683252cb89aa17b6f6cd600
dshaw1242 <david.shaw1242@gmail.com>
2021-07-15 08:47:36 -0600

    fixed error so service work installs correctly, added service work activation function

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,12 @@
-const APP_PREFIX = 'BudgetTracker-';
-const VERSION = 'v1';
-const CACHE_NAME = APP_PREFIX + VERSION;
+const CACHE_NAME = 'budget-tracker-v1'
+const DATA_CACHE_NAME = 'data-budget-tracker-v1';
 
 // files to cache
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/manifest.json',
-    '/serivce-worker.js',
+    '/service-worker.js',
     '/css/styles.css',
     '/icons/icon-72x72.png',
     '/icons/icon-96x96.png',
@@ -24,11 +23,28 @@ const FILES_TO_CACHE = [
 // install service worker
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Your files were cached successfully');
-                return cache.addAll(FILES_TO_CACHE);
-            })
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Your files were pre-cached successfully');
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
     self.skipWaiting();
+});
+
+
+//  activate the serivce worker and remove old data from the cache
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if(key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('Removing old cache data', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
 });
