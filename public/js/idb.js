e4e499e45c42bf3bdbbed13f9d9bc477602517d3
dshaw1242 <david.shaw1242@gmail.com>
2021-07-15 13:23:39 -0600

    added indexDB script tag to index.html

---
+++ public/js/idb.js
@@ -0,0 +1,10 @@
+let db;
+
+const request = indexedDB.open('budget_tracker', 1);
+
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+
+    db.createObjectStore('')
+}
