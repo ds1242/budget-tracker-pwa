30ca1b99d43136fefa280b0222511b50068cf21c
dshaw1242 <david.shaw1242@gmail.com>
2021-07-15 13:29:20 -0600

    setup error handling for indexDB

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,5 +6,17 @@ const request = indexedDB.open('budget_tracker', 1);
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
 
-    db.createObjectStore('')
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if(navigator.onLine) {
+        // uploadTransaction();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
 }
