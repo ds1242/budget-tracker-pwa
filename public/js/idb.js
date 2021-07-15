c99a94c78a0360e44b03dd8318dcc79fd03ab85c
dshaw1242 <david.shaw1242@gmail.com>
2021-07-15 13:51:27 -0600

    setup indexDB to save transactions and then upload when back online

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,17 +6,64 @@ const request = indexedDB.open('budget_tracker', 1);
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
 
-    db.createObjectStore('new_transaction', { autoIncrement: true });
+    db.createObjectStore('new_budget_item', { autoIncrement: true });
 };
 
 request.onsuccess = function(event) {
     db = event.target.result;
 
     if(navigator.onLine) {
-        // uploadTransaction();
+        uploadTransaction();
     }
 };
 
 request.onerror = function(event) {
     console.log(event.target.errorCode);
 }
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_budget_item'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_budget_item');
+
+    budgetObjectStore.add(record);
+}
+
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new_budget_item'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_budget_item');
+
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0 ) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if(serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                const transaction = db.transaction(['new_budget_item'], 'readwrite');
+
+                const budgetObjectStore = transaction.objectStore('new_budget_item');
+
+                budgetObjectStore.clear();
+                console.log('All saved transactions have been submitted');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    };
+};
+
+window.addEventListener('online', uploadTransaction);
